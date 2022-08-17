203f574a43184d5e041485e4d4005bc9a8d57008
null-91 <sophiea0110@naver.com>
2022-08-18 00:59:39 +0900

    layout, 컨트롤 경로 구현2

--- src/main/java/com/studyCafe/controller/MemberController.java
+++ src/main/java/com/studyCafe/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.studyCafe.controller;
 
 
+import com.studyCafe.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,10 +15,16 @@ public class MemberController {
         return "members/login";
     }
 
-    @GetMapping("/members/join")
-    public String join(){
+    @GetMapping("members/join")
+    public String createForm(){
         return "/members/createMemberForm";
     }
 
+    @PostMapping(value = "members/join")
+    public String create(Member member){
+        System.out.println(member.getId());
+        System.out.println(member.getPw());
+        return "redirect:/";
+    }
 
 }
