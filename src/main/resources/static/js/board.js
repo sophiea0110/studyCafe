6913a32828eb2ad66d01ddeb673f72e12a5c0b8c
null-91 <sophiea0110@naver.com>
2022-09-21 22:53:52 +0900

    BoardList CSS 작업중

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,4 +1,3 @@
-/*
 function idCheck(){
     let MemberId = document.getElementById("MemberId")
     let write = document.getElementById("write")
@@ -10,24 +9,3 @@ function idCheck(){
         alert("로그인 상태를 확인하세요")
     }
 }
-*/
-/*
-function findBySearch(){
-    let searchOption = document.getElementById("search-kind")
-    let searchKind = searchOption.options[searchOption.selectedIndex].value
-    let searchWord = document.getElementById("searchWord").value;
-
-     $.ajax({
-        type: "get",
-        url: "/board/boardSearch",
-        data: {"searchKind" : searchKind, "searchWord" : searchWord},
-        success : function(data){
-            console.log(data)
-            console.log('통신 성공');
-        },
-        error: function(){
-            console.log('통신 에러');
-        }
-     })
-}
-*/
